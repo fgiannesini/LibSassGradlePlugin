6a7b0c8e13007950284aa8926d95beb4daa31906
Fabien GIANNESINI <fgiannesini@Shiva>
2016-04-01 01:21:59 +0200

    Test correction

--- LibSassGradlePlugin/src/test/java/com/fabien/giannesini/libsass/gradle/plugin/LibSassGradlePluginTest.java
+++ LibSassGradlePlugin/src/test/java/com/fabien/giannesini/libsass/gradle/plugin/LibSassGradlePluginTest.java
@@ -9,11 +9,10 @@ import com.github.fgiannesini.libsass.gradle.plugin.tasks.CompileLibSassTask;
 
 public class LibSassGradlePluginTest {
 
-    @Test
-    public void checkPlugin_Compile_Task() {
-        final Project project = ProjectBuilder.builder().build();
-        project.getPlugins().apply("com.libsass.gradle.plugin");
-        Assert.assertTrue(project.getTasks()
-                .getByName("compileLibSass") instanceof CompileLibSassTask);
-    }
+	@Test
+	public void checkPlugin_Compile_Task() {
+		final Project project = ProjectBuilder.builder().build();
+		project.getPlugins().apply("com.github.fgiannesini.libsass.gradle.plugin");
+		Assert.assertTrue(project.getTasks().getByName("compileLibSass") instanceof CompileLibSassTask);
+	}
 }
