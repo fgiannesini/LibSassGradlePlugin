dc89bc0094761285e1c3dd2bc4edde9482cc87bb
Fabien <Fabien@Bahamut>
2016-04-17 18:16:54 +0200

    Extract watcher code for continuous compilation. Manage subfolders watching

--- LibSassGradlePlugin/src/main/java/com/github/fgiannesini/libsass/gradle/plugin/tasks/CompileLibSassTaskDelegate.java
+++ LibSassGradlePlugin/src/main/java/com/github/fgiannesini/libsass/gradle/plugin/tasks/CompileLibSassTaskDelegate.java
@@ -23,126 +23,135 @@ import lombok.Getter;
  */
 public class CompileLibSassTaskDelegate {
 
-	/**
-	 * Current gradle project
-	 */
-	private final Project project;
+    /**
+     * Current gradle project
+     */
+    private final Project project;
 
-	/**
-	 * Lib sass compiler
-	 */
-	private final LibSassCompiler libSassCompiler;
+    /**
+     * Lib sass compiler
+     */
+    private final LibSassCompiler libSassCompiler;
 
-	@Getter
-	private File inputFile;
+    @Getter
+    private File inputFile;
 
-	private File outputFile;
+    private File outputFile;
 
-	@Getter
-	private Path watchedDirectoryPath;
+    public CompileLibSassTaskDelegate(final Project project,
+            final Logger logger) {
+        this.project = project;
+        this.libSassCompiler = new LibSassCompiler(logger);
+        this.init();
+    }
 
-	public CompileLibSassTaskDelegate(final Project project, final Logger logger) {
-		this.project = project;
-		this.libSassCompiler = new LibSassCompiler(logger);
-		this.init();
-	}
+    /**
+     * LibSass compilator parameters initialisation
+     */
+    private void init() {
+        LibSassParameters extension = this.project.getExtensions()
+                .findByType(LibSassParameters.class);
+        if (extension == null) {
+            extension = new LibSassParameters();
+        }
 
-	/**
-	 * LibSass compilator parameters initialisation
-	 */
-	private void init() {
-		LibSassParameters extension = this.project.getExtensions().findByType(LibSassParameters.class);
-		if (extension == null) {
-			extension = new LibSassParameters();
-		}
+        this.inputFile = this.getMandatoryFile(extension.getInputFilePath(),
+                "inputFilePath");
+        if (!this.inputFile.exists()) {
+            throw new IllegalArgumentException(
+                    this.inputFile.getPath() + " does not exist");
+        }
 
-		this.inputFile = this.getMandatoryFile(extension.getInputFilePath(), "inputFilePath");
-		if (!this.inputFile.exists()) {
-			throw new IllegalArgumentException(this.inputFile.getPath() + " does not exist");
-		}
+        this.outputFile = this.getMandatoryFile(extension.getOutputFilePath(),
+                "outputFilePath");
 
-		this.outputFile = this.getMandatoryFile(extension.getOutputFilePath(), "outputFilePath");
+        this.manageBoolean(extension.getSourceMapContents(),
+                this.libSassCompiler::setSourceMapContents);
 
-		this.manageBoolean(extension.getSourceMapContents(), this.libSassCompiler::setSourceMapContents);
+        this.manageBoolean(extension.getSourceMapEmbed(),
+                this.libSassCompiler::setSourceMapEmbed);
 
-		this.manageBoolean(extension.getSourceMapEmbed(), this.libSassCompiler::setSourceMapEmbed);
+        this.manageBoolean(extension.getSourceComments(),
+                this.libSassCompiler::setSourceComments);
 
-		this.manageBoolean(extension.getSourceComments(), this.libSassCompiler::setSourceComments);
+        this.manageBoolean(extension.getOmitSourceMappingURL(),
+                this.libSassCompiler::setOmitSourceMappingURL);
 
-		this.manageBoolean(extension.getOmitSourceMappingURL(), this.libSassCompiler::setOmitSourceMappingURL);
+        Optional.ofNullable(extension.getInputSyntax()).map(String::toUpperCase)
+                .map(PluginInputSyntax::valueOf)
+                .ifPresent(this.libSassCompiler::setInputSyntax);
 
-		Optional.ofNullable(extension.getInputSyntax()).map(String::toUpperCase).map(PluginInputSyntax::valueOf)
-				.ifPresent(this.libSassCompiler::setInputSyntax);
+        Optional.ofNullable(extension.getOutputStyle()).map(String::toUpperCase)
+                .map(PluginOutputStyle::valueOf)
+                .ifPresent(this.libSassCompiler::setOutputStyle);
 
-		Optional.ofNullable(extension.getOutputStyle()).map(String::toUpperCase).map(PluginOutputStyle::valueOf)
-				.ifPresent(this.libSassCompiler::setOutputStyle);
+        Optional.ofNullable(extension.getPrecision())
+                .ifPresent(this.libSassCompiler::setPrecision);
 
-		Optional.ofNullable(extension.getPrecision()).ifPresent(this.libSassCompiler::setPrecision);
+        Optional.ofNullable(extension.getIncludePaths())
+                .map(path -> path.split(File.pathSeparator))
+                .ifPresent(this.libSassCompiler::addIncludePaths);
 
-		Optional.ofNullable(extension.getIncludePaths()).map(path -> path.split(File.pathSeparator))
-				.ifPresent(this.libSassCompiler::addIncludePaths);
+        Optional.ofNullable(extension.getSourceMapFilePath())
+                .map(this::buildFile)
+                .ifPresent(this.libSassCompiler::setSourceMapFile);
+    }
 
-		Optional.ofNullable(extension.getSourceMapFilePath()).map(this::buildFile)
-				.ifPresent(this.libSassCompiler::setSourceMapFile);
+    /**
+     * Build file from currnt gradle project and relative path (from parameters)
+     *
+     * @param filePath
+     * @return
+     */
+    private File buildFile(final String filePath) {
+        return this.buildAbsolutePath(filePath).toFile();
+    }
 
-		final Optional<Path> watchDirectoryPathOptional = Optional.ofNullable(extension.getWatchedDirectoryPath())
-				.map(this::buildAbsolutePath);
-		if (watchDirectoryPathOptional.isPresent()) {
-			this.watchedDirectoryPath = watchDirectoryPathOptional.get();
-		}
-	}
+    /**
+     * Build absolute path from currnt gradle project and relative path (from
+     * parameters)
+     *
+     * @param filePath
+     * @return
+     */
+    private Path buildAbsolutePath(final String filePath) {
+        return Paths.get(this.project.getRootDir().getAbsolutePath(), filePath);
+    }
 
-	/**
-	 * Build file from currnt gradle project and relative path (from parameters)
-	 *
-	 * @param filePath
-	 * @return
-	 */
-	private File buildFile(final String filePath) {
-		return this.buildAbsolutePath(filePath).toFile();
-	}
+    /**
+     * Check and build file
+     *
+     * @param filePath
+     * @param parameterName
+     * @return
+     */
+    private File getMandatoryFile(final String filePath,
+            final String parameterName) {
+        if (filePath == null) {
+            throw new IllegalArgumentException(
+                    "Parameter " + parameterName + " should be set");
+        }
+        return this.buildFile(filePath);
+    }
 
-	/**
-	 * Build absolute path from currnt gradle project and relative path (from
-	 * parameters)
-	 *
-	 * @param filePath
-	 * @return
-	 */
-	private Path buildAbsolutePath(final String filePath) {
-		return Paths.get(this.project.getRootDir().getAbsolutePath(), filePath);
-	}
+    /**
+     * Map boolean parameter
+     *
+     * @param booleanValue
+     * @param booleanConsumer
+     */
+    private void manageBoolean(final Boolean booleanValue,
+            final Consumer<Boolean> booleanConsumer) {
+        Optional.ofNullable(booleanValue).map(Boolean::booleanValue)
+                .ifPresent(booleanConsumer);
+    }
 
-	/**
-	 * Check and build file
-	 *
-	 * @param filePath
-	 * @param parameterName
-	 * @return
-	 */
-	private File getMandatoryFile(final String filePath, final String parameterName) {
-		if (filePath == null) {
-			throw new IllegalArgumentException("Parameter " + parameterName + " should be set");
-		}
-		return this.buildFile(filePath);
-	}
-
-	/**
-	 * Map boolean parameter
-	 *
-	 * @param booleanValue
-	 * @param booleanConsumer
-	 */
-	private void manageBoolean(final Boolean booleanValue, final Consumer<Boolean> booleanConsumer) {
-		Optional.ofNullable(booleanValue).map(Boolean::booleanValue).ifPresent(booleanConsumer);
-	}
-
-	/**
-	 * Launche libsass compilation
-	 *
-	 * @throws CompilationException
-	 */
-	public void compile() throws CompilationException {
-		this.libSassCompiler.compileFile(this.inputFile, this.outputFile);
-	}
+    /**
+     * Launche libsass compilation
+     *
+     * @throws CompilationException
+     */
+    public void compile() throws CompilationException {
+        this.libSassCompiler.compileFile(this.inputFile, this.outputFile);
+    }
 }
--- LibSassGradlePlugin/src/main/java/com/github/fgiannesini/libsass/gradle/plugin/tasks/CompileLibSassWithWatchTask.java
+++ LibSassGradlePlugin/src/main/java/com/github/fgiannesini/libsass/gradle/plugin/tasks/CompileLibSassWithWatchTask.java
@@ -1,23 +1,12 @@
 package com.github.fgiannesini.libsass.gradle.plugin.tasks;
 
-import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.nio.file.Path;
-import java.nio.file.StandardWatchEventKinds;
-import java.nio.file.WatchEvent;
-import java.nio.file.WatchKey;
-import java.nio.file.WatchService;
-
 import org.gradle.api.DefaultTask;
-import org.gradle.api.logging.Logger;
 import org.gradle.api.tasks.TaskAction;
 
 import io.bit3.jsass.CompilationException;
 
 /**
- *
  * Continuous compilation by watching files modifications in a directory
- *
  */
 public class CompileLibSassWithWatchTask extends DefaultTask {
     @Override
@@ -27,81 +16,8 @@ public class CompileLibSassWithWatchTask extends DefaultTask {
 
     @TaskAction
     public void compileLibSass() throws CompilationException {
-        final Logger logger = this.getLogger();
-        final CompileLibSassTaskDelegate compileLibSassTaskDelegate = new CompileLibSassTaskDelegate(
-                this.getProject(), logger);
-
-        final Path watchedDirectory = this
-                .getWatchedDirectory(compileLibSassTaskDelegate);
-
-        try {
-            final WatchService watcher = FileSystems.getDefault()
-                    .newWatchService();
-
-            watchedDirectory.register(watcher,
-                    StandardWatchEventKinds.ENTRY_CREATE,
-                    StandardWatchEventKinds.ENTRY_DELETE,
-                    StandardWatchEventKinds.ENTRY_MODIFY);
-
-            logger.info("Watch Service registered for dir: "
-                    + watchedDirectory.getFileName());
-
-            while (true) {
-                WatchKey key;
-                try {
-                    key = watcher.take();
-                } catch (final InterruptedException ex) {
-                    logger.error("Wath Service interrupted", ex);
-                    return;
-                }
-
-                for (final WatchEvent<?> event : key.pollEvents()) {
-                    final WatchEvent.Kind<?> kind = event.kind();
-
-                    @SuppressWarnings("unchecked")
-                    final WatchEvent<Path> ev = (WatchEvent<Path>) event;
-                    final Path fileName = ev.context();
-
-                    logger.info(kind.name() + ": " + fileName);
-
-                    try {
-                        compileLibSassTaskDelegate.compile();
-                    } catch (final CompilationException ce) {
-                        logger.error(
-                                "Error during compilation, see above for details");
-                    }
-                }
-
-                final boolean valid = key.reset();
-                if (!valid) {
-                    break;
-                }
-            }
-
-        } catch (final IOException ex) {
-            logger.error("Problem on Watch Service", ex);
-        }
-    }
-
-    /**
-     * Check and build watched directory path
-     *
-     * @param compileLibSassTaskDelegate
-     * @return
-     */
-    private Path getWatchedDirectory(
-            final CompileLibSassTaskDelegate compileLibSassTaskDelegate) {
-        final Path watchedDirectory = compileLibSassTaskDelegate
-                .getWatchedDirectoryPath();
-        if (watchedDirectory == null) {
-            throw new IllegalArgumentException(
-                    "watchedDirectoryPath must be set");
-        }
-        if (!watchedDirectory.toFile().exists()) {
-            throw new IllegalArgumentException(
-                    watchedDirectory.toAbsolutePath().toString()
-                            + "doesn't exist");
-        }
-        return watchedDirectory;
+        final CompileLibSassWatcher watcher = new CompileLibSassWatcher(
+                this.getLogger(), this.getProject());
+        watcher.launchWatcher();
     }
 }
